1cc7f859a3d4c113c69986f7cc3ea5c56c2ff36e
oddoddo <eehd80yul@gmail.com>
2024-06-16 22:46:10 +0900

    style reset, variable, mixins 셋팅

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import Layout from '@/components/layout/Layout'
-import { AppProps } from 'next/app'
-import '@/styles/globals.css'
+import Layout from '@/components/layout/Layout';
+import { AppProps } from 'next/app';
+import '@/styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
-export default MyApp
+export default MyApp;
